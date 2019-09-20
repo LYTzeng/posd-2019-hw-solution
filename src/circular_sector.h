8da79437601c84245dfa78ab15d84c395f0ca73d
108598017 <t104820003@ntut.org.tw>
2019-09-20 15:18:34 +0800

    fix indentation

--- src/circular_sector.h
+++ src/circular_sector.h
@@ -1,22 +1,29 @@
 #ifndef CIRCULAR_SEC
 #define CIRCULAR_SEC
 #include <math.h>
+#include <stdio.h>
 #include "shape.h"
 
-class CircularSector: public Shape
+class CircularSector : public Shape
 {
-    public:
-        CircularSector(double radius, double degree): _radius(radius), _degree(degree){}
-        double area() const
-        {
-            return M_PI * pow(_radius, 2) * (_degree/360);
-        }
-        double perimeter() const
-        {
-            return 2 * M_PI * _radius * (_degree/360) + 2 * _radius;
-        }
+public:
+    CircularSector(double radius, double degree) : _radius(radius), _degree(degree) {}
+    double area() const
+    {
+        return M_PI * pow(_radius, 2) * (_degree / 360);
+    }
+    double perimeter() const
+    {
+        return 2 * M_PI * _radius * (_degree / 360) + 2 * _radius;
+    }
+    double sumOfSquares() const
+    {
+        double area = this->area();
+        double perimeter = this->perimeter();
+        return pow(area, 2) + pow(perimeter, 2);
+    }
 
-    private:
-        double _radius, _degree;
+private:
+    double _radius, _degree;
 };
 #endif
--- src/shape.h
+++ src/shape.h
@@ -4,10 +4,10 @@
 
 class Shape
 {
-    public:
-        // Virtual Methods
-        virtual double area() const = 0;
-        virtual double perimeter() const = 0;
-
+public:
+    // Virtual Methods
+    virtual double area() const = 0;
+    virtual double perimeter() const = 0;
+    virtual double sumOfSquares() const = 0;
 };
 #endif
