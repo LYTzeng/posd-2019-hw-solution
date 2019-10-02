3f0d6212722847fa0249332f0238f95022efc325
108598017 <t104820003@ntut.org.tw>
2019-10-02 09:18:21 +0800

    finished terminal.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,35 @@
-#include <gtest/gtest.h>
-#include "../test/ut_sort.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "terminal.h"
 
 int main(int argc, char **argv)
 {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    //bin/geo input.txt output.txt [area perimeter  sumOfSquares]  [inc dec]
+    if (argc < 5)
+    {
+        Terminal terminal("Unuseful User Input!");
+        terminal.showResult();
+        return 0;
+    }
+    else
+    {
+        std::string inputPath(argv[1]);
+        std::string outputPath(argv[2]);
+        std::string featureArg(argv[3]);
+        std::string orderingArg(argv[4]);
+
+        std::ifstream file;
+        file.open(inputPath);
+        std::string inputLine;
+        std::getline(file, inputLine);
+        file.close();
+
+        std::ofstream wrFile;
+        wrFile.open(outputPath);
+        Terminal terminal(inputLine + " " + featureArg + " " + orderingArg);
+        wrFile << terminal.showResult() << "\n";
+        wrFile.close();
+        return 0;
+    }
 }
---
+++ src/terminal.h
@@ -0,0 +1,183 @@
+#ifndef TERMINAL
+#define TERMINAL
+#include <string>
+#include <regex>
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <sstream>
+#include "../src/shape.h"
+#include "../src/ellipse.h"
+#include "../src/circular_sector.h"
+#include "../src/triangle.h"
+#include "../src/sort.h"
+
+class Terminal
+{
+public:
+    // you can use regular expression to get useful token from "Input string"
+    Terminal(const std::string input)
+    {
+        // Search for Shapes
+        _stringForSearch = input;
+        std::regex regexSearchAllShapes("(Ellipse|CircularSector)[\\s]{0,}\\([\\s]{0,}[0-9]{1,}.?[0-9]{0,}[\\s]{0,},[\\s]{0,}[0-9]{1,}.?[0-9]{0,}[\\s]{0,}\\)|"
+                                        "(Triangle)[\\s]{0,}\\("
+                                        "([\\s]{0,}-?[0-9]{1,}.?[0-9]{0,}[\\s]{0,},){5}"
+                                        "[\\s]{0,}-?[0-9]{1,}.?[0-9]{0,}[\\s]{0,}\\)");
+        std::smatch match;
+        while (std::regex_search(_stringForSearch, match, regexSearchAllShapes))
+        {
+            _shapesStr.push_back(match[0]);
+            _stringForSearch = match.suffix().str();
+        }
+        // Search for Digits
+        std::regex searchPositiveNumbers("[0-9]{1,}.?[0-9]{0,}");
+        std::regex searchPosAndNegNumbers("-?[0-9]{1,}.?[0-9]{0,}");
+        for (int i = 0; i < _shapesStr.size(); ++i)
+        {
+            if (std::regex_match(_shapesStr[i], std::regex("Ellipse.*")))
+            {
+                std::sregex_iterator it = std::sregex_iterator(_shapesStr[i].begin(), _shapesStr[i].end(), searchPositiveNumbers);
+                std::smatch sm;
+                sm = *it;
+                double semiMajorAxes = std::stod(sm.str(0));
+                it++;
+                sm = *it;
+                double semiMinorAxes = std::stod(sm.str(0));
+                _shapes.push_back(new Ellipse(semiMajorAxes, semiMinorAxes));
+            }
+            else if (std::regex_match(_shapesStr[i], std::regex("CircularSector.*")))
+            {
+                std::sregex_iterator it = std::sregex_iterator(_shapesStr[i].begin(), _shapesStr[i].end(), searchPositiveNumbers);
+                std::smatch sm;
+                sm = *it;
+                double radius = std::stod(sm.str(0));
+                it++;
+                sm = *it;
+                double degree = std::stod(sm.str(0));
+                _shapes.push_back(new CircularSector(radius, degree));
+            }
+            else if (std::regex_match(_shapesStr[i], std::regex("Triangle.*")))
+            {
+                std::vector<double> axis;
+                for (std::sregex_iterator it = std::sregex_iterator(_shapesStr[i].begin(), _shapesStr[i].end(), searchPosAndNegNumbers);
+                     it != std::sregex_iterator(); it++)
+                {
+                    std::smatch sm;
+                    sm = *it;
+                    axis.push_back(std::stod(sm.str(0)));
+                }
+                _shapes.push_back(new Triangle(axis[0], axis[1], axis[2], axis[3], axis[4], axis[5]));
+            }
+        }
+
+        _stringForSearch = input;
+    }
+
+    std::string showResult()
+    {
+        if (_stringForSearch == "Unuseful User Input!")
+        {
+            throw std::string("Unuseful User Input!");
+        }
+        std::smatch match;
+        // Search for feature
+        std::regex searchFeature(".*(area|perimeter|sumOfSquares).*");
+        if (!std::regex_search(_stringForSearch, match, searchFeature))
+            throw std::string("Unuseful User Input!");
+        _feature = match[1];
+
+        // Search for order
+        std::regex searchOrder(".*(inc|dec).*");
+        if (!std::regex_search(_stringForSearch, match, searchOrder))
+            throw std::string("Unuseful User Input!");
+        _order = match[1];
+
+        // Assign _shapes to _sortShapes
+        _sortShapes = new Sort(&_shapes);
+        // Contitions to identify which method to use.
+        if (_feature == "area" && _order == "inc")
+        {
+            AscendingComparison ascendingComparison("area");
+            _sortShapes->sortArea(ascendingComparison);
+        }
+        else if (_feature == "area" && _order == "dec")
+        {
+            DescendingComparison descendingComparison("area");
+            _sortShapes->sortArea(descendingComparison);
+        }
+        else if (_feature == "perimeter" && _order == "inc")
+        {
+            AscendingComparison ascendingComparison("perimeter");
+            _sortShapes->sortArea(ascendingComparison);
+        }
+        else if (_feature == "perimeter" && _order == "dec")
+        {
+            DescendingComparison descendingComparison("perimeter");
+            _sortShapes->sortArea(descendingComparison);
+        }
+        else if (_feature == "sumOfSquares" && _order == "inc")
+        {
+            AscendingComparison ascendingComparison("sumOfSquares");
+            _sortShapes->sortArea(ascendingComparison);
+        }
+        else if (_feature == "sumOfSquares" && _order == "dec")
+        {
+            DescendingComparison descendingComparison("sumOfSquares");
+            _sortShapes->sortArea(descendingComparison);
+        }
+
+        std::vector<Shape *>::iterator shapePtr;
+        shapePtr = _shapes.begin();
+        std::string values = "[";
+        std::ostringstream strs;
+        if (_feature == "area")
+        {
+            for (int i = 0; i < _shapes.size(); ++i)
+            {
+                Shape *temp = *(shapePtr + i);
+                if (i != _shapes.size() - 1)
+                    values += std::to_string(temp->area()) + ", ";
+                else
+                    values += std::to_string(temp->area()) + "]";
+            }
+            return values;
+        }
+        else if (_feature == "perimeter")
+        {
+            for (int i = 0; i < _shapes.size(); ++i)
+            {
+                Shape *temp = *(shapePtr + i);
+                if (i != _shapes.size() - 1)
+                    values += std::to_string(temp->perimeter()) + ", ";
+                else
+                    values += std::to_string(temp->perimeter()) + "]";
+            }
+            return values;
+        }
+        else if (_feature == "sumOfSquares")
+        {
+            for (int i = 0; i < _shapes.size(); ++i)
+            {
+                Shape *temp = *(shapePtr + i);
+                if (i != _shapes.size() - 1)
+                    values += std::to_string(temp->sumOfSquares()) + ", ";
+                else
+                    values += std::to_string(temp->sumOfSquares()) + "]";
+            }
+            return values;
+        }
+        else
+            return "NULL";
+    }
+
+private:
+    std::vector<std::string> _shapesStr;
+    std::vector<Shape *> _shapes;
+    Sort *_sortShapes;
+    std::string _feature;
+    std::string _order;
+    std::string _stringForSearch;
+};
+
+#endif
