41c565b35dbcd03d6e229adad967a8f717572338
Your name <you@example.com>
2019-10-19 15:29:52 +0800

    push test

--- src/complex_solids.h
+++ src/complex_solids.h
@@ -1,17 +1,17 @@
-// #ifndef COMPLEX_SOLID
-// #define COMPLEX_SOLID
+#ifndef COMPLEX_SOLID
+#define COMPLEX_SOLID
 
-// #include <vector>
-// #include "solid.h"
+#include <vector>
+#include "solid.h"
 
-// class ComplexSolids : public Solid
-// {
-//   public:
-//     ComplexSolids(std::vector<Solid *> *solids);
-//     double bottomArea() const override;
-//     double volume() const override;
+class ComplexSolids : public Solid
+{
+  public:
+    ComplexSolids(std::vector<Solid *> *solids);
+    double bottomArea() const override;
+    double volume() const override;
 
-//   private:
-// };
+  private:
+};
 
-// #endif
+#endif
